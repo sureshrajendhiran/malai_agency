f21e8b4f1487c9ea8ce07cf66b98ddc33092b5fd
sureshrajendhiran <sureshrajendhiranr@gmail.com>
2024-11-03 04:40:12 +0530

    cleaned

--- webapp/src/app/material-module.ts
+++ webapp/src/app/material-module.ts
@@ -31,7 +31,7 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatSelectModule} from '@angular/material/select';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSidenavModule} from '@angular/material/sidenav'; 
 import {MatSliderModule} from '@angular/material/slider';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
--- webapp/src/app/nav/nav.component.ts
+++ webapp/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -7,10 +8,26 @@ import { Component } from '@angular/core';
 })
 export class NavComponent {
 
-  navList=<any>[
-    {dname:'Home',route:'home',icon:'home'},
-    {dname:'Quotation',route:'qi/quotation',icon:'format_quote'},
-    {dname:'Invoice',route:'qi/invoice',icon:'monetization_on'},
-    {dname:'Master',route:'master/master',icon:'inventory_2'},
+  navList = <any>[
+    { dname: 'Home', route: 'home', icon: 'home' },
+    { dname: 'Quotation', route: 'qi/quotation', icon: 'format_quote' },
+    { dname: 'Invoice', route: 'qi/invoice', icon: 'monetization_on' },
+    { dname: 'Master', route: 'master/master', icon: 'inventory_2' },
   ]
+  userInfo: any;
+
+  constructor(private routingNavigate: Router) { }
+
+  ngOnInit(): void {
+    if (!!localStorage.getItem("user")) {
+      this.userInfo = JSON.parse(localStorage.getItem("user") || "")
+    }
+  }
+
+
+  signOut() {
+    localStorage.clear();
+    this.routingNavigate.navigate(['/login']);
+  }
+
 }
